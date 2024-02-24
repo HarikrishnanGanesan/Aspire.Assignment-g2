5412cd474239f8e65f222644551c9797b9b1f870
Hari Ganesan <hari.ganesan@aspiresys.com>
2024-02-24 14:43:23 +0530

    header

--- aspire-assignment-g2-master/Angular/Angular/src/app/app.module.ts
+++ aspire-assignment-g2-master/Angular/Angular/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginModule } from './login/login.module';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    LoginModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        LoginModule,
+        HttpClientModule,
+        HeaderComponent,
+        FooterComponent
+    ]
 })
 export class AppModule { }
